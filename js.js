ce9a329b34d0f827df3bd19c383fa4c3cc83d89c
ziadfaisal06-cmd <ziadfaisal06@gmail.com>
2026-05-04 15:46:03 +0300

    Update js.js

--- js.js
+++ js.js
@@ -2,11 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const toggleBtn = document.getElementById('theme-toggle');
     const body = document.body;
 
-    // استرجاع الثيم وحالة الزرار
     const savedTheme = localStorage.getItem('theme') || 'dark';
     body.setAttribute('data-theme', savedTheme);
     
-    // تحديث نص الزرار بناءً على الثيم
+    
     toggleBtn.innerText = savedTheme === 'dark' ? 'LIGHT MODE' : 'DARK MODE';
 
     toggleBtn.addEventListener('click', () => {
@@ -16,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
         body.setAttribute('data-theme', newTheme);
         localStorage.setItem('theme', newTheme);
         
-        // تغيير الكلمة في الزرار
+     
         toggleBtn.innerText = newTheme === 'dark' ? 'LIGHT MODE' : 'DARK MODE';
     });
 });
